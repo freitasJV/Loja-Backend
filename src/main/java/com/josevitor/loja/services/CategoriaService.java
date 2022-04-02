a1b58c5d30a26664eccabee41e56c18edadaca27
Jose Vitor <freitasjvdev@gmail.com>
2022-04-01 23:15:27 -0300

    Refactor packages

---
+++ src/main/java/com/josevitor/loja/services/CategoriaService.java
@@ -0,0 +1,45 @@
+package com.josevitor.loja.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import com.josevitor.loja.domain.Categoria;
+import com.josevitor.loja.repositories.CategoriaRepository;
+import com.josevitor.loja.services.exceptions.DataIntegrityException;
+import com.josevitor.loja.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
+	}
+}
