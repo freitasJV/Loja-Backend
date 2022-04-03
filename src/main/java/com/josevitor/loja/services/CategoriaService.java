d3993673d7b23ebad01e0647eed79fd966873433
Jose Vitor <freitasjvdev@gmail.com>
2022-04-03 10:58:13 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/josevitor/loja/services/CategoriaService.java
+++ src/main/java/com/josevitor/loja/services/CategoriaService.java
@@ -34,8 +34,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -46,6 +47,10 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 	public List<Categoria> findAll() {
